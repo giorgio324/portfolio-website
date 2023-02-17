dc5a3a9527413ec3d7be0d77889d75ba1e11ab0e
giorgi <giorgikochuashvili2002@gmail.com>
2023-02-18 01:59:00 +0400

    added useefect that adds a class when nav is open

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
+import { GiHamburgerMenu } from 'react-icons/gi';
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  // this adds nav-open when nav opens so overflow content will not be shown
+  useEffect(() => {
+    if (isNavOpen) {
+      document.body.classList.add('nav-open');
+    } else {
+      document.body.classList.remove('nav-open');
+    }
+  }, [isNavOpen]);
+
   return (
     <div className='navbar-container'>
-      <nav className='navbar'>
+      <nav className={isNavOpen ? 'navbar open' : 'navbar'}>
         <div className='toggle-button-container'>
-          <button>X</button>
+          <button onClick={() => setIsNavOpen(!isNavOpen)} className='nav-btn'>
+            <GiHamburgerMenu />
+          </button>
         </div>
         <div className='links-container'>
-          <Link to={'/'}>Home</Link>
-          <Link to={'/projects'}>Projects</Link>
-          <Link to={'/experience'}>Experience</Link>
+          <Link to={'/'} className='nav-link'>
+            Home
+          </Link>
+          <Link to={'/projects'} className='nav-link'>
+            Projects
+          </Link>
+          <Link to={'/experience'} className='nav-link'>
+            Experience
+          </Link>
         </div>
       </nav>
     </div>
